2502e9d1b82465f22acd47467e4581a24a638e5c
Miha Jamsek <miha_jamsek@windowslive.com>
2019-12-06 18:35:22 +0100

    fix for CD

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -4,6 +4,11 @@ set -e
 
 # docker build -t gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT .
 
+if [[ -z "${TEST_ENV}" ]]; then
+  echo "Google credentials not set!"
+  exit 233
+fi
+
 echo $GCLOUD_PASS | base64 --decode -i > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 

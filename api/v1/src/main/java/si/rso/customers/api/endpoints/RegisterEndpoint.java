e4df04ed2dacc01c9ea715c0eb4942304964817b
Miha Jamsek <miha_jamsek@windowslive.com>
2020-01-03 23:23:30 +0100

    added external logs

--- api/v1/src/main/java/si/rso/customers/api/endpoints/RegisterEndpoint.java
+++ api/v1/src/main/java/si/rso/customers/api/endpoints/RegisterEndpoint.java
@@ -1,14 +1,14 @@
 package si.rso.customers.api.endpoints;
 
+import com.kumuluz.ee.logs.LogManager;
+import com.kumuluz.ee.logs.Logger;
+import com.kumuluz.ee.logs.cdi.Log;
 import si.rso.customers.lib.AccountRegistration;
 import si.rso.customers.services.CustomerService;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -18,8 +18,16 @@ import javax.ws.rs.core.Response;
 @Produces(MediaType.APPLICATION_JSON)
 public class RegisterEndpoint {
     
+    private static final Logger LOG = LogManager.getLogger(RegisterEndpoint.class.getSimpleName());
+    
     @Inject
     private CustomerService customerService;
+    
+    @GET
+    @Log
+    public Response test() {
+        return Response.ok().build();
+    }
     
     @POST
     public Response registerUser(AccountRegistration accountRegistration) {

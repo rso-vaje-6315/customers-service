f173a7d57274705e12e7df9940a09762ae80d4ae
Miha Jamsek <miha_jamsek@windowslive.com>
2020-01-06 21:06:14 +0100

    added openapi annotation

--- api/v1/src/main/java/si/rso/customers/api/endpoints/RegisterEndpoint.java
+++ api/v1/src/main/java/si/rso/customers/api/endpoints/RegisterEndpoint.java
@@ -3,6 +3,8 @@ package si.rso.customers.api.endpoints;
 import com.kumuluz.ee.logs.LogManager;
 import com.kumuluz.ee.logs.Logger;
 import com.kumuluz.ee.logs.cdi.Log;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.eclipse.microprofile.metrics.annotation.Counted;
 import org.eclipse.microprofile.metrics.annotation.Timed;
 import si.rso.customers.lib.AccountRegistration;
@@ -29,6 +31,11 @@ public class RegisterEndpoint {
     @Inject
     private CustomerService customerService;
     
+    @Operation(description = "Registers user.",
+        summary = "registers user.", tags = "account",
+        responses = {
+            @ApiResponse(responseCode = "200", description = "User was created.")
+        })
     @POST
     @Timed(name = "register-user-time")
     @Counted(name = "register-user-count")
